330a686ef92633c623fc4f8f80efec86f22460f2
juliansibaja84 <juliansibaja84@gmail.com>
2018-07-12 09:44:11 -0500

    Added week_08 with initial project and Added node_modules to gitignore file

---
+++ week_08/C09/bookshelves-app/src/components/right-sidebar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../styles/right-sidebar.css';
+
+export default class RightSideBar extends Component {
+  render() {
+    return (
+      <div id="sidebar-right">
+      </div>
+    );
+  }
+}
---
+++ week_08/C09/bookshelves-app/src/config.js
@@ -0,0 +1,11 @@
+const PORT = 3000;
+
+const config = {
+  homepage: `http://127.0.0.1:${PORT}/home`,
+  apiBaseUrl: 'http://127.0.0.1:5001/api',
+
+};
+
+
+
+export default config;
